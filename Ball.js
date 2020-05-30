19f06ddebe9b606d91cff5300e9f7df7551aa3bc
skywalker-its <59409021+skywalker-its@users.noreply.github.com>
2020-05-30 19:16:00 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,20 +1,20 @@
 class Ball {
     constructor(x,y,radius) {
       var options ={
-        frictionAir: 0.4,
-        friction: 0,
-        restitution: 1
+        frictionAir: 0,
+        friction: 1,
+        restitution: 1.0
       }
       this.body = Bodies.circle(x,y,radius,options);
       this.radius = radius;
       World.add(world, this.body);
     }
-      //
+
     display() {
-        var pos = this.body.position;
+          var pos = this.body.position;
           var angle = this.body.angle;
           push();
-          translate(pos.x, pos.y);
+          translate(this.body.position.x, this.body.position.y);
           rotate(angle);
           fill("blue");
          ellipseMode(RADIUS);
